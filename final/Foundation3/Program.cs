710132060053590f8785c823a597f88aa82f377c
TimmyZ <106173110+tizufelt@users.noreply.github.com>
2023-05-31 21:16:33 -0400

    syntax update

--- final/Foundation3/Program.cs
+++ final/Foundation3/Program.cs
@@ -12,7 +12,7 @@ namespace EventPlanning
             List<Event> events = new List<Event>();
 
             // Add some activities to the list
-            DateTime schedule = new DateTime(2023, 07, 06, 12, 00, 00);
+            DateTime schedule = new DateTime(0000, 12, 25, 12, 00, 00);
             Address location = new Address("1600 Pennsylvania Avenue,", "Washington, D.C.", "USA", "37188");
             events.Add(new Lecture("My Favorite Spy Glass", "Lecture Description", schedule, schedule, location, "Sherlock Holmes", 500));
 
